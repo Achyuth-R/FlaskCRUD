c43e426058b1390ed253ae2b883705e547c1af1a
Achyuth-R <r.achyuth.99@gmail.com>
2022-11-10 22:58:32 +0530

    move db cred to ini file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask
+from app.module import DbConfig
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_URI = "postgresql://ApplicationRole:applicationpassword@localhost:5432/Dev_DB"
-
+# database_URI = "postgresql://ApplicationRole:applicationpassword@localhost:5432/Dev_DB"
+database_URI = DbConfig.Config()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
